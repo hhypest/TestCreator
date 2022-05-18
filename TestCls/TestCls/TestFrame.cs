e0307c4281f66d9a025686f4b8a1645b1cb9d6fe
hhypest <hypest3@yandex.ru>
2022-05-18 22:26:31 +0300

    ref

--- TestCls/TestCls/TestFrame.cs
+++ TestCls/TestCls/TestFrame.cs
@@ -5,31 +5,22 @@ namespace TestCls
     [ProtoContract(SkipConstructor = true)]
     public partial class TestFrame : object
     {
-        #region[Поля]
-
-        [ProtoMember(1, Name = "Name")]
-        private string _nametest;
-
-        [ProtoMember(2, Name = "Path")]
-        private string _pathtest;
-
-        [ProtoMember(3, Name = "Count")]
-        private int _count;
-
-        [ProtoMember(4, Name = "MultiOne")]
-        private bool _multione;
-
-        [ProtoMember(5, Name = "Ask")]
-        private readonly IDictionary<Guid, Ask> _list;
-
-        #endregion
-
         #region[Свойства]
 
-        public string NameTest { get => _nametest; set => _nametest = value; }
-        public string PathTest { get => _pathtest; set => _pathtest = value; }
-        public int Count { get => _count; set => _count = value; }
-        public bool Multione { get => _multione; set => _multione = value; }
+        [ProtoMember(1, Name = "Имя")]
+        public string NameTest { get; set; }
+
+        [ProtoMember(2, Name = "Путь")]
+        public string PathTest { get; set; }
+
+        [ProtoMember(3, Name = "Количество")]
+        public int Count { get; set; }
+
+        [ProtoMember(4, Name = "Мультиответ")]
+        public bool Multione { get; set; }
+
+        [ProtoMember(5, Name = "Словарь")]
+        private IDictionary<Guid, Ask> List { get; }
 
         #endregion
 
@@ -37,11 +28,11 @@ namespace TestCls
 
         public TestFrame(string name, bool multione)
         {
-            _nametest = name;
-            _multione = multione;
-            _pathtest = string.Empty;
-            _count = 0;
-            _list = new Dictionary<Guid, Ask>();
+            NameTest = name;
+            Multione = multione;
+            PathTest = string.Empty;
+            Count = 0;
+            List = new Dictionary<Guid, Ask>();
         }
 
         #endregion
@@ -51,12 +42,12 @@ namespace TestCls
         public void NewAsk(string asks, (string name, bool state)[] variants)
         {
             Ask ask = new(asks, variants);
-            if (!_list.ContainsKey(ask.Guid))
-                _list.Add(ask.Guid, ask);
+            if (!List.ContainsKey(ask.Guid))
+                List.Add(ask.Guid, ask);
             else
             {
-                _list.Remove(ask.Guid);
-                _list.Add(ask.Guid, ask);
+                List.Remove(ask.Guid);
+                List.Add(ask.Guid, ask);
             }
 
             Count++;
@@ -65,40 +56,40 @@ namespace TestCls
         public void NewAsk((string asks, (string name, bool state)[] variants) newitem)
         {
             Ask ask = new(newitem.asks, newitem.variants);
-            if (!_list.ContainsKey(ask.Guid))
-                _list.Add(ask.Guid, ask);
+            if (!List.ContainsKey(ask.Guid))
+                List.Add(ask.Guid, ask);
             else
             {
-                _list.Remove(ask.Guid);
-                _list.Add(ask.Guid, ask);
+                List.Remove(ask.Guid);
+                List.Add(ask.Guid, ask);
             }
 
             Count++;
         }
 
         public ParallelQuery<KeyValuePair<Guid, string>> GetTreeAsk()
-            => from KeyValuePair<Guid, Ask> key in _list.AsParallel()
-               select new KeyValuePair<Guid, string>(key.Key, key.Value.GetName());
+            => from KeyValuePair<Guid, Ask> key in List.AsParallel()
+               select new KeyValuePair<Guid, string>(key.Key, key.Value.Name);
 
         public ParallelQuery<KeyValuePair<string, (string name, bool state)[]>> GetTesterList()
-            => from KeyValuePair<Guid, Ask> key in _list.AsParallel()
-               select new KeyValuePair<string, (string name, bool state)[]>(key.Value.GetName(), key.Value.GetVariants());
+            => from KeyValuePair<Guid, Ask> key in List.AsParallel()
+               select new KeyValuePair<string, (string name, bool state)[]>(key.Value.Name, key.Value.GetVariants());
 
         public ParallelQuery<(string name, bool state)> GetListAsk(Guid tag)
-            => _list[tag].GetVariants().AsParallel();
+            => List[tag].GetVariants().AsParallel();
 
         public (string asks, (string name, bool state)[] variants) GetEditAsk(Guid tag)
-            => (_list[tag].GetName(), _list[tag].GetVariants());
+            => (List[tag].Name, List[tag].GetVariants());
 
         public void RemoveAsk(Guid tag)
         {
-            _list.Remove(tag);
+            List.Remove(tag);
             Count--;
         }
 
         public void RemoveAsk()
         {
-            _list.Clear();
+            List.Clear();
             Count = 0;
         }
 
@@ -109,37 +100,26 @@ namespace TestCls
         [ProtoContract]
         private struct Ask
         {
-            [ProtoMember(1, Name = "ID")]
-            private Guid _guid;
+            [ProtoMember(1, Name = "Ключ")]
+            public Guid Guid { get; set; }
 
-            [ProtoMember(2, Name = "Ask")]
-            private string _name;
+            [ProtoMember(2, Name = "Вопрос")]
+            public string Name { get; set; }
 
-            [ProtoMember(3, Name = "Variants")]
-            private Variant[] _variants;
+            [ProtoMember(3, Name = "Количество")]
+            public int Count { get; set; }
 
-            [ProtoMember(4, Name = "CountVariants")]
-            private int _count;
-
-            public Guid Guid { get => _guid; set => _guid = value; }
-            public string Name { get => _name; set => _name = value; }
-            private Variant[] Variants { get => _variants; set => _variants = value; }
-            public int Count { get => _count; set => _count = value; }
+            [ProtoMember(4, Name = "Варианты")]
+            private Variant[] Variants { get; set; }
 
             public Ask(string name, (string var, bool state)[] variants) : this()
             {
-                _name = name;
-                _guid = Guid.NewGuid();
-                _variants = Array.ConvertAll<(string name, bool state), Variant>(variants,
+                Name = name;
+                Guid = Guid.NewGuid();
+                Variants = Array.ConvertAll<(string name, bool state), Variant>(variants,
                                                                                  a => new Variant(a));
-                _count = _variants.Length;
+                Count = Variants.Length;
             }
-
-            public Guid GetGuid()
-                => Guid;
-
-            public string GetName()
-                => Name;
 
             public (string var, bool state)[] GetVariants()
                 => Array.ConvertAll(Variants, a => a.GetVariant());
@@ -149,19 +129,16 @@ namespace TestCls
             [ProtoContract]
             private struct Variant
             {
-                [ProtoMember(1, Name = "ThisVariant")]
-                private string _name;
+                [ProtoMember(1, Name = "Вариант")]
+                public string Name { get; set; }
 
-                [ProtoMember(2, Name = "ThisLogical")]
-                private bool _state;
-
-                public string Name { get => _name; set => _name = value; }
-                public bool State { get => _state; set => _state = value; }
+                [ProtoMember(2, Name = "Булево")]
+                public bool State { get; set; }
 
                 public Variant((string name, bool state) tuple) : this()
                 {
-                    _name = tuple.name;
-                    _state = tuple.state;
+                    Name = tuple.name;
+                    State = tuple.state;
                 }
 
                 public (string name, bool state) GetVariant()
